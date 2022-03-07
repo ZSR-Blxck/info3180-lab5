31f6bffa1d5e68bed099aa5de33798519227d6b8
ZSR-Blxck <zavierrattray@gmail.com>
2022-03-07 00:01:28 -0500

    Step8

--- app/views.py
+++ app/views.py
@@ -31,11 +31,13 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
             # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
@@ -43,11 +45,20 @@ def login():
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
 
-            # get user id, load into session
-            login_user(user)
+            user = UserProfile.query.filter_by(username=username).first()
 
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+             if user is not None and check_password_hash(user.password, password):
+
+                # get user id, load into session
+                login_user(user)
+
+                # remember to flash a message to the user
+                flash('Logged in successfully.', 'success')
+                return redirect(url_for('secure_page'))
+            else:
+                flash('Invalid Username/Password.') 
+                return redirect(url_for("home"))  
+                # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 

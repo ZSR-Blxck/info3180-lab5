f3f183f2a3d5e62613f2856f38190d152a6bfbad
ZSR-Blxck <zavierrattray@gmail.com>
2022-04-02 04:09:59 -0500

    Creating Database

--- app/config.py
+++ app/config.py
@@ -7,6 +7,10 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')or 'postgresql://username:password@localhost/databasename'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')or 'mysql://user:password@localhost/database'
 
+    SQLALCHEMY_BINDS = {
+        'db2': 'mysql://user:pass@localhost/database2'
+        #'db3': 'mysql://user:pass@localhost/database3'
+}
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
--- app/models.py
+++ app/models.py
@@ -12,15 +12,26 @@ class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
-    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(255))
+    history=db.Column(db.String(255))
+    date_=db.Column(db.DATETIME)
+    status=db.Column(db.String(25))
+    symptom_id=db.Column(db.Integer,foreign_key=('symptom.id'))
+    ailment_id=db.Column(db.Integer,foreign_key=('ailment.id'))
 
-    def __init__(self, first_name, last_name, username, password):
+    def __init__(self, first_name, last_name, username, password,history,date_,status,symptom_id,ailment_id):
         self.first_name = first_name
         self.last_name = last_name
-        self.username = username
+        self.email = email
         self.password = generate_password_hash(
             password, method='pbkdf2:sha256')
+        self.history=history
+        self.date_=date_
+        self.status=status
+        self.symptom_id=symptom_id
+        self.ailment_id=ailment_id
+
 
     def is_authenticated(self):
         return True
@@ -39,3 +50,69 @@ class UserProfile(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+
+class Symptoms(db.Model):
+    __bind_key__='db2'
+    __tablename__='symptoms'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80))
+    desc = db.Column(db.String(255))
+    types = db.Column(db.String(20))
+    priority = db.Column(db.String(40))
+
+    def __init__(self,name,desc,types,priority):
+        self.name=name
+        self.desc=desc
+        self.types=types
+        self.priority=priority
+
+
+class Ailment(db.Model):
+    __bind_key__='db3'
+    __tablename__='ailment'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80))
+    sci_name = db.Column(db.String(80))
+    desc = db.Column(db.String(255))
+    types = db.Column(db.String(20))
+    severity = db.Column(db.String(40))
+    symptom_id = db.Column(db.Integer,foreign_key=('symptoms.id'))
+    treatment_id = db.Column(db.Integer,foreign_key=('treatments.id'))
+
+    def __init__(self,name,sci_name,desc,types,severity,symptom_id,treatment_id):
+        self.name=name
+        self.sci_name=sci_name
+        self.desc=desc
+        self.types=types
+        self.severity=severity
+        self.symptom_id=symptom_id
+        self.treatment_id=treatment_id
+
+class Treatments(db.Model):
+    __bind_key__='db4'
+    __tablename__='treatments'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80))
+    details = db.Column(db.String(255)) 
+
+    def __init__(self,name,details):
+        self.name = name
+        self.details = details
+
+class Emer_contacts(db.Model):
+    __bind_key__='db5'
+    __tablename__ = 'emer_contacts'
+    id = db.Column(db.Integer,primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    phone = db.Column(db.String(15))
+    relationship = db.Column(db.String(15))
+    user_id = db.Column(db.Integer,foreign_key=UserProfile.id)
+    
+    def __init__(self,first_name,last_name,phone,relationship,user_id):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.phone=phone
+        self.relationship=relationship
+        self.user_id=user_id
+
